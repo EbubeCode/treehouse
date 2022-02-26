0291c5946226ae9d5c4a20669c241bba8b3fab8e
EbubeCode <chukwuma258@gmail.com>
2022-02-26 13:50:22 +0100

    Initial commit

---
+++ src/test/java/com/garden/treehouse/TreehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garden.treehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
