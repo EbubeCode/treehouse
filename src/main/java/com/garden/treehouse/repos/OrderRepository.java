bbd6a4299746c89b62a014df8520ba95a767c5f4
EbubeCode <chukwuma258@gmail.com>
2022-04-15 01:54:30 +0100

    fix bug'

--- src/main/java/com/garden/treehouse/repos/OrderRepository.java
+++ src/main/java/com/garden/treehouse/repos/OrderRepository.java
@@ -4,9 +4,10 @@ import com.garden.treehouse.model.Order;
 import com.garden.treehouse.model.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface OrderRepository extends CrudRepository<Order, Long>{
 
-    Optional<Order> findOrderByUserAndOrderStatus(User user, String status);
+    Optional<List<Order>> findOrderByOrderStatus(String status);
 }
--- src/main/java/com/garden/treehouse/services/OrderService.java
+++ src/main/java/com/garden/treehouse/services/OrderService.java
@@ -11,69 +11,77 @@ import java.util.List;
 
 @Service
 @Transactional
-public class OrderService{
+public class OrderService {
 
-	private final OrderRepository orderRepository;
-	private final CartItemService cartItemService;
+    private final OrderRepository orderRepository;
+    private final CartItemService cartItemService;
 
-	public OrderService(OrderRepository orderRepository, CartItemService cartItemService) {
-		this.orderRepository = orderRepository;
-		this.cartItemService = cartItemService;
-	}
+    public OrderService(OrderRepository orderRepository, CartItemService cartItemService) {
+        this.orderRepository = orderRepository;
+        this.cartItemService = cartItemService;
+    }
 
-	public Order createOrder(ShoppingCart shoppingCart,
-										  ShippingAddress shippingAddress,
-										  String shippingMethod,
-										  User user) {
+    public Order createOrder(ShoppingCart shoppingCart,
+                             ShippingAddress shippingAddress,
+                             String shippingMethod,
+                             User user) {
 
-		Order order = new Order();
-		order.setOrderStatus("pending");
-		order.setShippingAddress(shippingAddress);
-		order.setShippingMethod(shippingMethod == null ? "Ground": shippingMethod);
-		
-		List<CartItem> cartItemList = cartItemService.findByShoppingCart(shoppingCart);
-		
-		for(CartItem cartItem : cartItemList) {
-			Product product = cartItem.getProduct();
-			cartItem.setOrder(order);
-			if (product.getInStockNumber() < cartItem.getQty())
-				return null;
-			product.setInStockNumber(product.getInStockNumber() - cartItem.getQty());
-			if (product.getInStockNumber()  <= 0)
-				product.setActive(false);
+        Order order = new Order();
+        order.setOrderStatus("pending");
+        order.setShippingAddress(shippingAddress);
+        System.out.println("====================================================================================");
+        System.out.println(shippingMethod);
+        order.setShippingMethod(shippingMethod == null ? "Ground" : shippingMethod);
 
-		}
-		
-		order.setCartItemList(cartItemList);
-		order.setOrderDate(Calendar.getInstance().getTime());
-		order.setOrderTotal(shoppingCart.getGrandTotal());
-		shippingAddress.setOrder(order);
-		order.setUser(user);
-		order = orderRepository.save(order);
-		
-		return order;
-	}
+        List<CartItem> cartItemList = cartItemService.findByShoppingCart(shoppingCart);
 
-	public void deleteOrder(User user) {
-		var opOrder = orderRepository.findOrderByUserAndOrderStatus(user, "pending");
-		if(opOrder.isPresent()) {
-			var order = opOrder.get();
-			order.getCartItemList().forEach(c -> {
-				var newQty = c.getProduct().getInStockNumber() + c.getQty();
-				c.getProduct().setInStockNumber(newQty);
-				c.getProduct().setActive(true);
-			});
-			order = orderRepository.save(order);
-			orderRepository.delete(order);
-		}
-	}
+        for (CartItem cartItem : cartItemList) {
+            Product product = cartItem.getProduct();
+            cartItem.setOrder(order);
+            if (product.getInStockNumber() < cartItem.getQty())
+                return null;
+            product.setInStockNumber(product.getInStockNumber() - cartItem.getQty());
+            if (product.getInStockNumber() <= 0)
+                product.setActive(false);
 
-	public Order findById(Long id) {
-		return orderRepository.findById(id).orElse(null);
-	}
+        }
 
-	public Order save(Order order) {
-		return orderRepository.save(order);
-	}
+        order.setCartItemList(cartItemList);
+        order.setOrderDate(Calendar.getInstance().getTime());
+        order.setOrderTotal(shoppingCart.getGrandTotal());
+        shippingAddress.setOrder(order);
+        order.setUser(user);
+        order = orderRepository.save(order);
+
+        return order;
+    }
+
+    public void deleteOrder(User user) {
+        var opOrder = orderRepository.findOrderByOrderStatus("pending");
+        if (opOrder.isPresent()) {
+            var orders = opOrder.get();
+            var order = orders.stream()
+                    .filter(o -> o.getUser().getId().equals(user.getId()))
+                    .findFirst()
+                    .orElse(null);
+            if (order != null) {
+                order.getCartItemList().forEach(c -> {
+                    var newQty = c.getProduct().getInStockNumber() + c.getQty();
+                    c.getProduct().setInStockNumber(newQty);
+                    c.getProduct().setActive(true);
+                });
+                order = orderRepository.save(order);
+                orderRepository.delete(order);
+            }
+        }
+    }
+
+    public Order findById(Long id) {
+        return orderRepository.findById(id).orElse(null);
+    }
+
+    public Order save(Order order) {
+        return orderRepository.save(order);
+    }
 
 }
